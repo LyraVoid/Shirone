2c16b5869fc29e02c959f021f2171cc1ca3286e3
matsuzaka-yuki <matsuzaka-yuki@users.noreply.github.com>
2026-09-02 20:12:46 +0800

    ci(backend): test sqlite and postgres

--- backend/internal/database/database_test.go
+++ backend/internal/database/database_test.go
@@ -2,11 +2,30 @@ package database
 
 import (
 	"context"
+	"os"
 	"testing"
 )
 
 func TestOpenSQLite(t *testing.T) {
 	db, client, err := Open("sqlite", "file:database_test?mode=memory&cache=shared&_pragma=foreign_keys(1)")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+	if err := client.Schema.Create(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+	if err := db.PingContext(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestOpenPostgres(t *testing.T) {
+	dsn := os.Getenv("TEST_POSTGRES_URL")
+	if dsn == "" {
+		t.Skip("TEST_POSTGRES_URL is not configured")
+	}
+	db, client, err := Open("postgres", dsn)
 	if err != nil {
 		t.Fatal(err)
 	}
